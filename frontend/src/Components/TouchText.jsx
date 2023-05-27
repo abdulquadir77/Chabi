1c43ac86d92f56100bf758569a8b85ce768a1f48
abdulquadir77 <abdulquadir77@yahoo.com>
2023-05-27 13:56:43 +0530

    home page Ui design completed

---
+++ frontend/src/Components/TouchText.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./touchtext.css";
+
+const TouchText = () => {
+  return (
+    <>
+      <div id="container">
+        <h1>asdfjkl</h1>
+      </div>
+    </>
+  );
+};
+
+export default TouchText;
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import "./home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div id="container">
+        <h1>asdfjkl</h1>
+        <input type="text" placeholder="Re-Type" id="input" />
+      </div>
+    </>
+  );
 };
 
 export default Home;
